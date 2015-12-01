bdd81b712126c37616cf51a1e31ff9cfa3c81ce5
Stan Devproject <stan.devproject@gmail.com>
2015-12-01 12:34:59 +0200

    Добавлен AJAX и сортировка для фильтров

--- js/pictures.js
+++ js/pictures.js
@@ -8,6 +8,9 @@
   // Контейнер, куда сохраняются сгенерированные шаблоны.
   var container = document.querySelector('.pictures');
 
+  // Массив, который будет хранить загруженный список картинок.
+  var loadedPicturesData;
+
   // Таймаут ожидания загрузки, после которого загрузка считается несостоявшейся.
   var LOAD_TIMEOUT = 10000;
 
@@ -18,12 +21,114 @@
     // Скрываем блок с фильтрами.
     picturesFiltersHide();
 
-    pictures.forEach(function(picture) {
-      container.appendChild(getElementFromTemplate(picture));
-    });
+    // Устанавливаем обработчик, который следит за переключениями фильтров списка фотографий.
+    filtersForm.onchange = function() {
+      // Получаем текущее значение радиобаттона (т.е. имя фильтра, оно изменилось)
+      var selectedFilter = [].filter.call(filtersForm['filter'], function(item) {
+        return item.checked;
+      })[0].value;
+
+      setActivePictureFilter(selectedFilter);
+    };
+
+    // Генерируем и отображаем картинки на основе шаблонов.
+    getPicturesAndRender();
 
     // Показываем блок с фильтрами.
     picturesFiltersShow();
+  }
+
+  /**
+   * Установка выбранного фильтра картинок.
+   */
+  function setActivePictureFilter(selectedFilter) {
+    // Если загруженный массив с картинками скачан.
+    if (loadedPicturesData) {
+      // Копирование массива.
+      var filteredPictures = loadedPicturesData.slice(0);
+
+      switch (selectedFilter) {
+        case 'popular':
+          // Массив остается такой же, так как с такой сортировкой
+          // нам приходят данные с сервера.
+          break;
+        case 'new':
+          filteredPictures.sort(function(a, b) {
+            var aDate = new Date(a.date);
+            var bDate = new Date(b.date);
+
+            return bDate.getTime() - aDate.getTime();
+          });
+          break;
+        case 'discussed':
+          filteredPictures.sort(function(a, b) {
+            return b.comments - a.comments;
+          });
+          break;
+      }
+
+      picturesRender(filteredPictures);
+    }
+  }
+
+  /**
+   * Скачивает с сервера данные со списком картинок и их параметрами.
+   */
+  function getPicturesAndRender() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'data/pictures.json');
+    xhr.timeout = LOAD_TIMEOUT;
+
+    // Активируем прелоадер, который пропадет после успешного рендеринга шаблонов.
+    if (!container.classList.contains('pictures-loading')) {
+      container.classList.add('pictures-loading');
+    }
+
+    xhr.onload = function(evt) {
+      var rawData = evt.target.response;
+      loadedPicturesData = JSON.parse(rawData);
+      picturesRender(loadedPicturesData);
+    };
+
+    // Функция-обработчик ошибки загрузки.
+    var loadError = function() {
+      // Убираем прелоадер, если есть.
+      if (container.classList.contains('pictures-loading')) {
+        container.classList.remove('pictures-loading');
+      }
+
+      // Добавляем класс, сообщающий об ошибке.
+      if (!container.classList.contains('pictures-failure')) {
+        container.classList.add('pictures-failure');
+      }
+    };
+
+    xhr.onerror = loadError;
+    xhr.onabort = loadError;
+    xhr.ontimeout = loadError;
+
+    xhr.send();
+  }
+
+  /**
+   * Функция, ответственная за рендеринг картинок на основе шаблонов.
+   */
+  function picturesRender(pictures) {
+    // Очищаем содержимое контейнера.
+    container.innerHTML = '';
+
+    var fragment = document.createDocumentFragment();
+
+    pictures.forEach(function(picture) {
+      fragment.appendChild(getElementFromTemplate(picture));
+    });
+
+    // Данные сгенерированы успешно, убираем прелоадер, если он есть.
+    if (container.classList.contains('pictures-loading')) {
+      container.classList.remove('pictures-loading');
+    }
+
+    container.appendChild(fragment);
   }
 
   /**
